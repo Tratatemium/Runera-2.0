6854ff60a3e6db26731be4b7ea18bfb476713d09
Vladislav Zhuravskii <visitanoinvitado@gmail.com>
2026-08-19 14:00:25 +0200

    feat: add RunRequest and RunApi interfaces, update Run type definitions, and comment out unused code

--- packages/shared/types/runs/runs.types.ts
+++ packages/shared/types/runs/runs.types.ts
@@ -1,4 +1,4 @@
-import type { Dispatch, SetStateAction } from "react";
+// import type { Dispatch, SetStateAction } from "react";
 
 interface Run {
   runId: string;
@@ -27,68 +27,19 @@ interface Run {
 
 type RunsState = Record<string, Run>;
 
-interface RunsContextValue {
-  runs: RunsState | undefined;
-  isHydaratingRuns: boolean;
-  setIsHydratingRuns: Dispatch<SetStateAction<boolean>>;
-  hydrateRunsState: (runs: RunsState) => void;
-  clearRunsState: () => void;
-  postNewRunState: (newRun: Run) => void;
-  updateRunState: (updatedRun: Run) => void;
-  deleteRunState: (id: string) => void;
-}
-
-interface RunApi {
-  runId: string;
-  userId: string;
-  startTime: string;
-  date: string;
-  durationSec: number;
-  distanceMeters: number;
-  paceSecPerKm: number;
-  createdAt: string;
-  updatedAt: string;
-  title?: string;
-  notes?: string;
-  perceivedEffort?: number;
-  weather?:
-    | "sunny"
-    | "partly_cloudy"
-    | "cloudy"
-    | "rain"
-    | "snow"
-    | "windy"
-    | "hot"
-    | "cold";
-}
-
-type RunApiResponse = { runData: RunApi };
-type MyRunsApiResponse = { myRuns: RunApi[] };
-
-interface RunData {
-  startTime: string;
-  durationSec: number;
-  distanceMeters: number;
-  title?: string;
-  notes?: string;
-  perceivedEffort?: number;
-  weather?:
-    | "sunny"
-    | "partly_cloudy"
-    | "cloudy"
-    | "rain"
-    | "snow"
-    | "windy"
-    | "hot"
-    | "cold";
-}
+// interface RunsContextValue {
+//   runs: RunsState | undefined;
+//   isHydaratingRuns: boolean;
+//   setIsHydratingRuns: Dispatch<SetStateAction<boolean>>;
+//   hydrateRunsState: (runs: RunsState) => void;
+//   clearRunsState: () => void;
+//   postNewRunState: (newRun: Run) => void;
+//   updateRunState: (updatedRun: Run) => void;
+//   deleteRunState: (id: string) => void;
+// }
 
 export type {
   Run,
   RunsState,
-  RunsContextValue,
-  RunApi,
-  RunApiResponse,
-  MyRunsApiResponse,
-  RunData,
+  // RunsContextValue,
 };
