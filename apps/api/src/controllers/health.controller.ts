703c85f01d2c7d3830787b5a8a8fa5b9370cedaf
Vladislav Zhuravskii <visitanoinvitado@gmail.com>
2026-08-28 12:47:41 +0200

    feat: update health controller to use async/await for database connection

--- apps/api/src/controllers/health.controller.ts
+++ apps/api/src/controllers/health.controller.ts
@@ -3,12 +3,13 @@ import type { Request, Response } from "express";
 import { checkDBConnection } from "../utils/db.utils.js";
 import { getUptime } from "../utils/server.utils.js";
 
-function healthController(req: Request, res: Response) {
+async function healthController(req: Request, res: Response) {
+  const DBConnection = await checkDBConnection();
   res.status(200).json({
     status: "running",
     uptime: getUptime(),
     version: "1.0.0",
-    DBConnection: checkDBConnection(),
+    DBConnection,
   });
 }
 
