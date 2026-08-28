a22115a0a49f43626db752720930f8651c5bbfdf
Vladislav Zhuravskii <visitanoinvitado@gmail.com>
2026-08-28 20:53:01 +0200

    feat: wrap UserLayout component with RequireAuth for authentication

--- apps/web/src/app/user/layout.tsx
+++ apps/web/src/app/user/layout.tsx
@@ -1,11 +1,14 @@
+import { RequireAuth } from "@/components/auth";
 import bg from "@/assets/bg1.png";
 
 import styles from "./layout.module.css";
 
 export default function UserLayout({ children }: LayoutProps<"/user">) {
   return (
-    <div className={styles.layout} style={{ backgroundImage: `url(${bg})` }}>
-      {children}
-    </div>
+    <RequireAuth>
+      <div className={styles.layout} style={{ backgroundImage: `url(${bg})` }}>
+        {children}
+      </div>
+    </RequireAuth>
   );
 }
