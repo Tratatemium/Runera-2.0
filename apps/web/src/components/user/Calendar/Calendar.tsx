c2f968f25d283c4b1fbb393c467f8379c99d12fd
Vladislav Zhuravskii <visitanoinvitado@gmail.com>
2026-09-09 17:58:37 +0200

    Feat: implement CalendarDayButton component with styles for calendar interaction

--- apps/web/src/components/user/Calendar/Calendar.tsx
+++ apps/web/src/components/user/Calendar/Calendar.tsx
@@ -1,14 +1,20 @@
 "use client";
 
+import { useState } from "react";
 import { DayPicker } from "react-day-picker";
 import "react-day-picker/style.css";
 
 import { Panel } from "@/components/ui";
-import { CalendarDayButton } from "./CalendarDayButton";
+import { CalendarDayButton } from "./CalendarDayButton/CalendarDayButton";
 
 import styles from "./Calendar.module.css";
 
 function Calendar() {
+  const [selctedDay, setSelectedDay] = useState<Date | null>(null);
+  const handleDayClick = (date: Date) => {
+    setSelectedDay(date);
+  };
+
   return (
     <Panel variant="frostedAccent" className={styles.panel}>
       <h2 className={styles.heading}>Running calendar</h2>
@@ -16,7 +22,9 @@ function Calendar() {
         mode="single"
         weekStartsOn={1}
         components={{ DayButton: CalendarDayButton }}
+        onDayClick={handleDayClick}
       />
+      {selctedDay && selctedDay.toISOString()}
     </Panel>
   );
 }
