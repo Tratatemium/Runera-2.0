2d679646f2e602cb50eda11de078e9f716e8f56f
Vladislav Zhuravskii <visitanoinvitado@gmail.com>
2026-09-13 23:21:43 +0200

    feat: create RunItemFull component and associated styles, update RunPage layout and add run validation

--- apps/web/src/app/user/runs/[runId]/page.tsx
+++ apps/web/src/app/user/runs/[runId]/page.tsx
@@ -1,17 +1,41 @@
 import type { Run } from "@runera/shared";
 
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+
+import { useRunsContext } from "@/context/RunsContext";
+import { useRuns } from "@/hooks";
 import { Panel } from "@/components/ui";
 
 import styles from "./page.module.css";
 
 interface RunPageProps {
-  run: Run;
+  params: Promise<{
+    runId: string;
+  }>;
 }
 
-export default function RunPage({ run }: RunPageProps) {
+export default async function RunPage({ params }: RunPageProps) {
+  const { runId } = await params;
+
+  const router = useRouter();
+  const { runs, isHydratingRuns } = useRunsContext();
+  const { loading, loadingRunId, deleteRun } = useRuns();
+
+  useEffect(() => {
+    if (!runId || !runs) return;
+    if (!runs[runId]) {
+      router.push("/not-found");
+      return;
+    }
+  }, [router, runId, runs]);
+  if (!runs || !runId) return null;
+  const run = runs[runId];
+  if (!run) return null;
+
   return (
-    <main>
-      <Panel variant="gradientAccent"></Panel>
+    <main className={styles.main}>
+      <Panel variant="gradientAccent" className={styles.panel}></Panel>
     </main>
   );
 }
